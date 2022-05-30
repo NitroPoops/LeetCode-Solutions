d56d779ad2b76d6c6869dcc4a9149c2de1566808
nitro <57934981+NitroPoops@users.noreply.github.com>
2022-05-30 11:56:19 +0800

    Add files via upload

---
+++ Solutions/2042. Check If Numbers Are Ascending In A Sentence.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/check-if-numbers-are-ascending-in-a-sentence/
+
+class Solution:
+    def areNumbersAscending(self, s: str) -> bool:
+        r = [int(i) for i in s.split() if i.isdigit()]
+        return r == sorted(list(set(r)))
