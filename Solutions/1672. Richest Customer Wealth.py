46c4a0832baf400296109a3738b55da1dfe12839
nitro <57934981+NitroPoops@users.noreply.github.com>
2022-06-08 20:51:46 +0800

    Add files via upload

---
+++ Solutions/1672. Richest Customer Wealth.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/richest-customer-wealth/
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        return max(sum(i) for i in accounts)
