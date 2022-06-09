473debabaae95baeba3995dd5053a7d846a4ceca
nitro <57934981+NitroPoops@users.noreply.github.com>
2022-06-09 12:04:30 +0800

    Add files via upload

---
+++ Solutions/2011. Final Value of Variable After Performing Operations.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        n = 0
+
+        for i in operations:
+            if "x" in i:
+                n += 1
+            else:
+                n -= 1
+
+        return n
