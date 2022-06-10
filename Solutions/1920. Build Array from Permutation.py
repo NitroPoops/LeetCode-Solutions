fd5d8e9baf23e75d5eb314f66ef071cd245ea5de
nitro <57934981+NitroPoops@users.noreply.github.com>
2022-06-10 19:36:17 +0800

    Update 1920. Build Array from Permutation.py

--- Solutions/1920. Build Array from Permutation.py
+++ Solutions/1920. Build Array from Permutation.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/build-array-from-permutation/
 
+from typing import List
+
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
         return [nums[i] for i in nums]
