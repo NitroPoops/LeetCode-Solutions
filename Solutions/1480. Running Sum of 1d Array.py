16faad9b64ec4710b7bcabe2d35880391e09db09
nitro <57934981+NitroPoops@users.noreply.github.com>
2022-06-10 19:35:34 +0800

    Update 1480. Running Sum of 1d Array.py

--- Solutions/1480. Running Sum of 1d Array.py
+++ Solutions/1480. Running Sum of 1d Array.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/running-sum-of-1d-array/
 
+from typing import List
+
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         return [sum(nums[:idx + 1]) for idx in range(len(nums))]
