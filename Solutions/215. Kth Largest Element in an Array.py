50ddd81fed74d902132c30f11a3ed8e842429346
nitro <57934981+NitroPoops@users.noreply.github.com>
2022-06-10 19:38:16 +0800

    Update 215. Kth Largest Element in an Array.py

--- Solutions/215. Kth Largest Element in an Array.py
+++ Solutions/215. Kth Largest Element in an Array.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/kth-largest-element-in-an-array/
 
+from typing import List
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         return sorted(nums)[len(nums) - k]
