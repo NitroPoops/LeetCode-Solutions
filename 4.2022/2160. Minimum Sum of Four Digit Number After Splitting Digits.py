80ae84a7a42f01e7113cba912e7cf37169624566
nitro <57934981+NitroPoops@users.noreply.github.com>
2022-06-17 21:38:41 +0800

    Add files via upload

---
+++ 4.2022/2160. Minimum Sum of Four Digit Number After Splitting Digits.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/minimum-sum-of-four-digit-number-after-splitting-digits/
+
+class Solution:
+    def minimumSum(self, num: int) -> int:
+        num = sorted(list(str(num)))
+        
+        return int(num[0] + num[2]) + int(num[1] + num[3])
