9cf8db9dab22d76c8191cfe06ae3706d8f00770c
nitro <57934981+NitroPoops@users.noreply.github.com>
2022-07-21 20:32:49 +0800

    Add files via upload

---
+++ 5.2022/2103. Rings and Rods.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/rings-and-rods/
+
+class Solution:
+    def countPoints(self, rings: str) -> int:
+        count = 0
+
+        for i in range(10):
+            i = str(i)
+            if "R" + i in rings and "G" + i in rings and "B" + i in rings:
+                count += 1
+                
+        return count
