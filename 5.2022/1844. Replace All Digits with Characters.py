078eb4c60b6d20444a6f697a6eb87efb38264213
nitro <57934981+NitroPoops@users.noreply.github.com>
2022-08-02 22:20:00 +0800

    Add files via upload

---
+++ 5.2022/1844. Replace All Digits with Characters.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/replace-all-digits-with-characters/
+
+class Solution:
+    def replaceDigits(self, s: str) -> str:
+        s = list(s)
+        
+        for i in range(1, len(s), 2):
+            s[i] = chr(ord(s[i-1]) + int(s[i]))
+            
+        return "".join(s)
